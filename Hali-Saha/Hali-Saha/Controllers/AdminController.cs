908123416ce62ee61e5c202e1a1eaef27c9b1889
beyzanurodabas <83145431+beyzanurodabas@users.noreply.github.com>
2022-12-24 23:32:22 +0300

    .

--- Hali-Saha/Hali-Saha/Controllers/AdminController.cs
+++ Hali-Saha/Hali-Saha/Controllers/AdminController.cs
@@ -1,14 +1,43 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccess.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 
 namespace Hali_Saha.Controllers
 {
     [Authorize(Roles="Admin")]
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly DbHaliSahaContext _context;
+        private readonly ILogger<AdminController> _logger;
+        private readonly IStringLocalizer<AdminController> _localizer;
+
+        public AdminController(DbHaliSahaContext context, ILogger<AdminController> logger, IStringLocalizer<AdminController> localizer)
         {
+            _context = context;
+            _logger = logger;
+            _localizer = localizer;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
             return View();
         }
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddDays(1) }
+            );
+
+            return LocalRedirect(returnUrl);
+        }
+
+
     }
 }
